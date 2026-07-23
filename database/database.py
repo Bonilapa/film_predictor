bfb4b1d7ddaaeb249a2be368361603c63459a909
bonilap <intmain@bk.ru>
2026-07-23 19:25:27 +0300

    Начальная форма проекта

---
+++ database/database.py
@@ -0,0 +1,20 @@
+class DvdDatabase:
+    '''
+    Класс определяет все методы взаимодействия с базой данных
+    '''
+    def __init__(self) -> None:
+        self.dataset = None
+        pass
+
+    def form_csv(self):
+        '''
+        формирует датасет с актуальными данными
+        '''
+        pass
+
+    def save_csv(self, filename='rent7.csv'):
+        '''
+        Сохраняет сформированный датасет в файл
+        '''
+        pass
+    
